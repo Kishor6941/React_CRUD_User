083ea54a5fe5a433feaf0f44cb9a8f8655ffac3b
Vaibhavi7499 <vaibhavibodke13@gmail.com>
2024-08-05 23:23:12 +0530

    Lazy loading in react js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './components/Home';
-import AddEditUser from './components/AddEditUser';
-import ViewUserDetails from './components/ViewUserDetails';
+// import Home from './components/Home';
+// import AddEditUser from './components/AddEditUser';
+// import ViewUserDetails from './components/ViewUserDetails';
+
+const Home=React.lazy(()=>import('./components/Home'))
+const AddEditUser =React.lazy(()=>import('./components/AddEditUser'))
+const ViewUserDetails=React.lazy(()=>import('./components/ViewUserDetails'))
 
 
  const App = () => {
@@ -11,12 +15,14 @@ import ViewUserDetails from './components/ViewUserDetails';
     <>
       <BrowserRouter>
         <Layout />
+          <Suspense fallback={<div>Loading...</div>}>
         <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/add-user' element={<AddEditUser/>} />
         <Route path='/edit-user/:id' element={<AddEditUser/>} />
         <Route path='/view-user/:id' element={<ViewUserDetails/>} />
         </Routes>
+        </Suspense>
       </BrowserRouter>
     </>
   )
